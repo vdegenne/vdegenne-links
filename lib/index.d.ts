b5bbf730570313f89803080f952b77fadf4ce45a
vdegenne <vdegenne@gmail.com>
2023-10-13 10:23:54 +0200

    1

---
+++ lib/index.d.ts
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright (c) 2023 Valentin Degenne
+ * SPDX-License-Identifier: MIT
+ */
+/**
+ * Google related links
+ */
+export declare function googleImagesUrl(input: string): string;
+export declare function googleImagesOpen(input: string): void;
+export declare function googleTranslateUrl(input: string): string;
+export declare function googleTranslateOpen(input: string): void;
+/**
+ * Japanese related
+ */
+export declare function jishoUrl(input: string): string;
+export declare function jishoOpen(input: string): void;
+export declare function gooUrl(input: string): string;
+export declare function goo(input: string): void;
+export declare function weblioUrl(input: string): string;
+export declare function weblioOpen(input: string): void;
+export declare function kotobankUrl(input: string): string;
+export declare function kotobankOpen(input: string): void;
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,72 @@
+/**
+ * @license
+ * Copyright (c) 2023 Valentin Degenne
+ * SPDX-License-Identifier: MIT
+ */
+
+function open(url: string, newTab = true) {
+	if (newTab) {
+		window.open(url, '_blank');
+	} else {
+		window.open(url);
+	}
+}
+
+/**
+ * Google related links
+ */
+// google images
+export function googleImagesUrl(input: string) {
+	return `http://www.google.com/search?q=${encodeURIComponent(input)}&tbm=isch`;
+}
+export function googleImagesOpen(input: string) {
+	open(googleImagesUrl(input));
+}
+
+// google translate
+export function googleTranslateUrl(input: string) {
+	return `https://translate.google.com/?sl=auto&tl=en&text=${encodeURIComponent(
+		input
+	)}&op=translate`;
+}
+export function googleTranslateOpen(input: string) {
+	open(googleTranslateUrl(input));
+}
+
+/**
+ * Japanese related
+ */
+// jisho
+export function jishoUrl(input: string) {
+	return `https://jisho.org/search/${encodeURIComponent(input)}`;
+}
+export function jishoOpen(input: string) {
+	open(jishoUrl(input));
+}
+
+// goo
+export function gooUrl(input: string) {
+	// return `https://dictionary.goo.ne.jp/word/${encodeURIComponent(word)}/`;
+	return `https://dictionary.goo.ne.jp/srch/all/${encodeURIComponent(
+		input
+	)}/m0u/`;
+}
+export function goo(input: string) {
+	open(gooUrl(input));
+}
+
+// weblio
+export function weblioUrl(input: string) {
+	return `https://www.weblio.jp/content/${encodeURIComponent(input)}`;
+}
+export function weblioOpen(input: string) {
+	open(weblioUrl(input));
+}
+
+// kotobank
+export function kotobankUrl(input: string) {
+	return `https://kotobank.jp/word/${encodeURIComponent(input)}`;
+}
+export function kotobankOpen(input: string) {
+	open(kotobankUrl(input));
+}
